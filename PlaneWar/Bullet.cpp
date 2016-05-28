59fcd06206765c0a12846adf9fa7a1400e21465f
Ferris <fynet86@gmail.com>
2016-05-29 00:37:18 +0800

    增加Bullet 类

--- PlaneWar/Bullet.cpp
+++ PlaneWar/Bullet.cpp
@@ -7,3 +7,20 @@
 //
 
 #include "Bullet.hpp"
+
+Bullet::Bullet()
+{
+    if (!texture.loadFromImage(image)) {
+        return EXIT_FAILURE;
+    }
+    this->setTexture(texture);
+    this->setTextureRect(sf::IntRect(1004, 987,9, 21));
+}
+void Bullet::setuse(bool b)
+{
+    use = b;
+}
+bool Bullet::getuse()
+{
+    return use;
+}
--- PlaneWar/Bullet.hpp
+++ PlaneWar/Bullet.hpp
@@ -9,6 +9,23 @@
 #ifndef Bullet_hpp
 #define Bullet_hpp
 
-#include <stdio.h>
+#include "ResourcePath.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+class Bullet: public sf::Sprite
+{
+public:
+    Bullet();
+    static sf::Image image;
+    //    static void load();
+    void setuse(bool);
+    bool getuse();
+    void check();
+private:
+    sf::Texture  texture;
+    bool use = false;
+    
+};
+
 
 #endif /* Bullet_hpp */
