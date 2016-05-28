02d6aa1669a2cba57b2c33b720194be4659bfe67
Ferris <fynet86@gmail.com>
2016-05-29 00:35:28 +0800

    增加Weapon 类

--- PlaneWar/Weapon.cpp
+++ PlaneWar/Weapon.cpp
@@ -7,3 +7,40 @@
 //
 
 #include "Weapon.hpp"
+
+bool Weapon::fire(sf::Event event,sf::Vector2u plane_size, sf::Sprite*plane)
+{
+    
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::LAlt) {
+        bullets.push_back(new Bullet());
+        sf::Vector2f plane_position = plane->getPosition();
+        (*(bullets.end()-1))->setPosition(plane_size.x/2+plane_position.x,plane_position.y-20);
+        
+        return true;
+    }
+    return false;
+}
+void Weapon::fly()
+{
+    for(auto &a:bullets)
+    {
+        a->move(direction, -3);
+    }
+}
+void Weapon::fly2()
+{
+    for(auto &a:bullets)
+    {
+        a->move(direction, speed);
+    }
+}
+
+
+bool Weapon::fire2(sf::Vector2u plane_size, sf::Sprite*plane)
+{
+    bullets.push_back(new Bullet());
+    (*(bullets.end()-1))->setColor(sf::Color::Black);
+    sf::Vector2f plane_position = plane->getPosition();
+    (*(bullets.end()-1))->setPosition(plane_size.x/2+plane_position.x,plane_position.y+50);
+    return true;
+}
--- PlaneWar/Weapon.hpp
+++ PlaneWar/Weapon.hpp
@@ -9,6 +9,26 @@
 #ifndef Weapon_hpp
 #define Weapon_hpp
 
-#include <stdio.h>
+#include "Bullet.hpp"
+class Weapon
+{
+public:
+    friend class  Plane;
+    Weapon(float i):direction(i){}
+    bool fire(sf::Event,sf::Vector2u,sf::Sprite*);
+    bool fire2(sf::Vector2u plane_size, sf::Sprite*plane);
+    void fly();
+    void fly2();
+    void setspeed(int i)
+    {
+        speed = i;
+    }
+    std::vector<Bullet*> bullets;
+private:
+    //    MyPlane *myplane;
+    float i = 0;
+    int direction = 0;
+    int speed = 1;
+};
 
 #endif /* Weapon_hpp */
